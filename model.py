BIG FIX: remove w generation from input, it was not causal

model.py
@@ -53,6 +53,8 @@ class HydraLatentAttention(nn.Module):
 		self.qkv_u = SignedLinear(d_in, 3*n_qkv) # (embd, 3*qkv); transpose to (3*qkv, embd)
 		self.gate = SignedLinear(3*self.n_embd, d_in//2) # (qkv//2, in)
 		self.out = SignedLinear(d_in, 2*d_out) # (qkv, 2*out); view to (2*qkv, out) transpose to (out, 2*qkv)
+		self.t1 = SignedLinear(config.n_embd * config.n_head, config.n_embd)
+		self.t2 = SignedLinear(2*config.d_model, config.n_embd)
 
 	# https://arxiv.org/abs/2405.04434
 	# deepseek mla implementation without decoupled rope,
@@ -111,6 +113,8 @@ class HydraLatentAttention(nn.Module):
 		w_qkv_u = self.qkv_u(w_qkv_l).T.contiguous()
 		w_gate = self.gate(w_qkv_u.view(-1, 3*self.n_embd)).view(-1, self.d_in)
 		w_out = self.out(w_gate).view(-1, self.d_out).T.contiguous()
+		w = self.t1(w_out).T.contiguous()
+		w = self.t2(w)
 
 		# calculate query, key, values for all heads in batch and move head forward to be the batch dim
 		c_q, c_kv = F.linear(norm(x), w_qkv_l).chunk(2, dim=-1) # `c_kv` will be stored in the KV cache
@@ -127,7 +131,7 @@ class HydraLatentAttention(nn.Module):
 
 		# interleave heads of ela & aft
 		y = torch.stack([ela, aft], dim=3).flatten(2, 3).view(B, T, -1) # (B, T, 2*nh, hs) -> (B, T, 2K)
-		return F.linear(norm(y), w_out), w_out
+		return F.linear(norm(y), w_out), w
 
 class Silia(nn.Module):
 	def __init__(self, config: Config):
@@ -135,38 +139,14 @@ class Silia(nn.Module):
 		# two-thirds trick for hidden dimension to keep compute constant
 		self.a1 = HydraLatentAttention(config, config.n_embd, 2*config.d_model)
 		self.a2 = HydraLatentAttention(config, config.d_model, config.n_embd)
-		self.t1 = SignedLinear(config.n_embd * config.n_head, config.n_embd)
-		self.t2 = SignedLinear(2*config.d_model, config.n_embd)
+		self.w = nn.Linear(config.n_embd, config.n_embd, bias=False).weight
 
-	def forward(self, x, w, cos_sin):
+	def forward(self, x, cos_sin):
 		y, w = self.a1(x, w, cos_sin)
-		w = self.t1(w).T.contiguous()
-		w = self.t2(w)
-
 		u, v = y.chunk(2, dim=-1)
 		y = u * F.silu(v)
-
 		y, _ = self.a2(y, w, cos_sin)
 		return x + y
-
-class Block(nn.Module):
-	def __init__(self, config: Config):
-		super().__init__()
-		self.n_embd = config.n_embd
-		self.transform = nn.Linear(config.n_embd, config.n_embd*3, bias=False)
-		self.silia = Silia(config)
-
-	def forward(self, x, w0, cos_sin):
-		t, r, a = self.transform(x).chunk(3, dim=-1)
-
-		# generate a (C, C) matrix from input `x`
-		# compact b & c of shape (B, T, C) -> (B, C, C) -> (C, C) shape
-		s = 1 / math.sqrt(r.size(1))
-		w = r.transpose(1, 2) @ a * s
-		w = w.mean(dim=0)
-		w = norm(w) + w0
-
-		return self.silia(t, w, cos_sin), w
 
 class Valentine(nn.Module):
 	def __init__(self, config: Config):
@@ -177,7 +157,7 @@ class Valentine(nn.Module):
 
 		# factorized token embeddings
 		self.embed = nn.Embedding(config.vocab_size, config.n_embd)
-		self.blocks = nn.ModuleList([Block(config) for _ in range(config.n_layer)])
+		self.blocks = nn.ModuleList([Silia(config) for _ in range(config.n_layer)])
 		self.unembed = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 		self.embed.weight = self.unembed.weight
 
@@ -212,9 +192,8 @@ class Valentine(nn.Module):
 		x = self.embed(idx)
 		x = norm(x)
 
-		w = torch.zeros(self.config.n_embd, self.config.n_embd)
 		for block in self.blocks:
-			x, w = block(x, w, cos_sin)
+			x = block(x, cos_sin)
 
 		# forward the lm_head (compute logits)
 		x = norm(x)